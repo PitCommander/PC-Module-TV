d163e2e666ee28111ed7f4d67cb97897ec75ff5d
Preston <childrep@vt.edu>
2017-05-17 21:36:53 -0400

    Initial File Commit
    Screens for countdown timer, schedule, team logo, robot specs, and video
    stream

---
+++ README.md
@@ -0,0 +1,15 @@
+Polymer + electron package for Pit Commander UI
+
+Install package:
+
+    npm install
+
+Post install script should automatically call `polymer install` and install all bower dependencies.
+
+Assuming successful installation, in-browser version should be startable via 
+
+    polymer serve --open   
+
+Electron version should be startable via 
+
+    npm start
---
+++ announce.js
@@ -0,0 +1,95 @@
+var zmq = require('zeromq'),
+    sock = zmq.socket('sub');
+
+sock.connect('tcp://10.0.0.7:5800');
+sock.subscribe('');
+
+
+console.log('running announce');
+
+sock.on('message', function (message) {
+    //console.log(message);
+    var messageObj = JSON.parse(message);
+
+    switch (messageObj.id) {
+        case 'TimeTick':
+
+            var seconds = messageObj.payload.timeToZero % 60;
+            var minutes = Math.floor(messageObj.payload.timeToZero / 60);
+            var hours = Math.floor(minutes / 60);
+            var timeString = '';
+
+            if (hours != 0) {
+                minutes = minutes % 60;
+
+                timeString = hours + 'h ' + minutes + 'm ' + seconds + 's';
+            } else {
+                timeString = minutes + 'm ' + seconds + 's';
+            }
+
+            document.querySelector('timer-view').ttz = timeString;
+            break;
+        case 'MatchContainerUpdate':
+            var data = messageObj.payload.container;
+            document.querySelector('timer-view').nextNum = data.currentMatch.comp_level.toUpperCase() + data.currentMatch.match_number;
+            document.querySelector('schedule-view').record = data.wins + "-" + data.losses + "-" + data.ties;
+
+            var date = new Date(data.currentMatch.time * 1000);
+            var predictedDate = new Date((data.currentMatch.time + 500) * 1000);
+            document.querySelector('timer-view').scheduledTime = date.toLocaleTimeString([], {
+                hour: '2-digit',
+                minute: '2-digit'
+            });
+            //document.querySelector('timer-view').predictedTime = predictedDate.toLocaleTimeString();
+
+            var redTeam = data.currentMatch.redTeams;
+            var blueTeam = data.currentMatch.blueTeams;
+
+            var redPos = redTeam.indexOf('frc401');
+            var bluePos = blueTeam.indexOf('frc401');
+
+            var ally1, ally2, ally3, oppo1, oppo2, oppo3 = '';
+
+            if (redPos != -1) {
+                ally1 = redTeam[0].split('c')[1];
+                ally2 = redTeam[1].split('c')[1];
+                ally3 = redTeam[2].split('c')[1];
+
+                oppo1 = blueTeam[0].split('c')[1];
+                oppo2 = blueTeam[1].split('c')[1];
+                oppo3 = blueTeam[2].split('c')[1];
+
+                document.querySelector('timer-view').bumperColor = 'red';
+            } else if (bluePos != -1) {
+                ally1 = blueTeam[0].split('c')[1];
+                ally2 = blueTeam[1].split('c')[1];
+                ally3 = blueTeam[2].split('c')[1];
+
+                oppo1 = redTeam[0].split('c')[1];
+                oppo2 = redTeam[1].split('c')[1];
+                oppo3 = redTeam[2].split('c')[1];
+
+                document.querySelector('timer-view').bumperColor = 'blue';
+            }
+
+            var allyString = ally1 + ' ' + ally2 + ' ' + ally3;
+            var oppoString = oppo1 + ' ' + oppo2 + ' ' + oppo3;
+
+            document.querySelector('timer-view').allies = allyString;
+            document.querySelector('timer-view').oppo = oppoString;
+
+            var schedule = messageObj.payload.container.schedule;
+
+            for (var match in schedule) {
+                var tmpDate = new Date(match.time * 1000);
+                var matchString = tmpDate.toLocaleTimeString([], {
+                    hour: '2-digit',
+                    minute: '2-digit'
+                });
+                match.timeString = matchString;
+            }
+
+            document.querySelector('schedule-view').data = schedule;
+            break;
+    }
+});
