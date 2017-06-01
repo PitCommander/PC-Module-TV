b36c150a30f7674e8262853e4a9efadce4ff98d7
Preston <childrep@vt.edu>
2017-05-31 22:14:41 -0400

    Added system volume control, unified parameters for video players, improved stream control logic.

--- announce.js
+++ announce.js
@@ -1,132 +1,131 @@
+var vol = require('vol');
 var zmq = require('zeromq'),
   sock = zmq.socket('sub');
 
 sock.connect('tcp://10.0.0.4:5800');
 sock.subscribe('');
-
-var testing = zmq.socket('sub');
-testing.connect('tcp://10.0.0.4:5802');
-testing.subscribe('');
-
-testing.on('message', function (message) {
+sock.on('message', function (message) {
   var messageObj = JSON.parse(message);
-  switch (messageObj.id) {
-  case 'TV_SET':
-    var data = messageObj.payload;
-    console.log(data);
-    var tv = document.querySelector('tv-app');
-    console.log(tv.screen);
-    if (tv.screen == data.name) {
-      console.log('name matched!')
-      tv.set('page', data.selected);
-      //powerHandlder(data.power);
-      //muteHandler(data.mute);
-      //volumeHandler(data.volume)
-    }
+  var data = messageObj.payload;
 
+  var tv = document.querySelector('tv-app');
+  var timerView = document.querySelector('timer-view');
+  var scheduleView = document.querySelector('schedule-view');
+  var streamView = document.querySelector('stream-view');
+
+  switch (messageObj.id) {
+  case 'TimeTick':
+    timeTickHandler(data, timerView);
+    break;
+  case 'MatchContainerUpdate':
+    data = data.container;
+    matchContainerHandler(data, timerView, scheduleView)
+    break;
+  case 'TV_SET':
+    tvHandler(data, tv);
+    break;
+  case 'GeneralContainerUpdate':
+    data = data.container;
+    generalContainerHandler(data, streamView);
     break;
   }
 });
 
-sock.on('message', function (message) {
-  var messageObj = JSON.parse(message);
-  switch (messageObj.id) {
-  case 'TimeTick':
+function timeTickHandler(data, timerView) {
+  var seconds = data.timeToZero % 60;
+  var minutes = Math.floor(data.timeToZero / 60);
+  var hours = Math.floor(minutes / 60);
+  var timeString = '';
 
-    var seconds = messageObj.payload.timeToZero % 60;
-    var minutes = Math.floor(messageObj.payload.timeToZero / 60);
-    var hours = Math.floor(minutes / 60);
-    var timeString = '';
+  if (hours != 0) {
+    minutes = minutes % 60;
 
-    if (hours != 0) {
-      minutes = minutes % 60;
+    timeString = hours + 'h ' + minutes + 'm ' + seconds + 's';
+  } else {
+    timeString = minutes + 'm ' + seconds + 's';
+  }
 
-      timeString = hours + 'h ' + minutes + 'm ' + seconds + 's';
-    } else {
-      timeString = minutes + 'm ' + seconds + 's';
-    }
+  timerView.ttz = timeString;
+}
 
-    document.querySelector('timer-view').ttz = timeString;
-    break;
-  case 'MatchContainerUpdate':
-    var data = messageObj.payload.container;
-    document.querySelector('timer-view').nextNum = data.currentMatch.comp_level.toUpperCase() + data.currentMatch.match_number;
-    document.querySelector('schedule-view').record = data.wins + "-" + data.losses + "-" + data.ties;
+function matchContainerHandler(data, timerView, scheduleView) {
+  timerView.nextNum = data.currentMatch.comp_level.toUpperCase() + data.currentMatch.match_number;
+  scheduleView.record = data.wins + "-" + data.losses + "-" + data.ties;
 
-    var date = new Date(data.currentMatch.time * 1000);
-    var predictedDate = new Date((data.currentMatch.time + 500) * 1000);
-    document.querySelector('timer-view').scheduledTime = date.toLocaleTimeString([], {
+  var date = new Date(data.currentMatch.time * 1000);
+  var predictedDate = new Date((data.currentMatch.time + 500) * 1000);
+  timerView.scheduledTime = date.toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit'
+  });
+  //timerView.predictedTime = predictedDate.toLocaleTimeString();
+
+  var redTeam = data.currentMatch.redTeams;
+  var blueTeam = data.currentMatch.blueTeams;
+
+  var redPos = redTeam.indexOf('frc401');
+  var bluePos = blueTeam.indexOf('frc401');
+
+  var ally1, ally2, ally3, oppo1, oppo2, oppo3 = '';
+
+  if (redPos != -1) {
+    ally1 = redTeam[0].split('c')[1];
+    ally2 = redTeam[1].split('c')[1];
+    ally3 = redTeam[2].split('c')[1];
+
+    oppo1 = blueTeam[0].split('c')[1];
+    oppo2 = blueTeam[1].split('c')[1];
+    oppo3 = blueTeam[2].split('c')[1];
+
+    timerView.bumperColor = 'red';
+  } else if (bluePos != -1) {
+    ally1 = blueTeam[0].split('c')[1];
+    ally2 = blueTeam[1].split('c')[1];
+    ally3 = blueTeam[2].split('c')[1];
+
+    oppo1 = redTeam[0].split('c')[1];
+    oppo2 = redTeam[1].split('c')[1];
+    oppo3 = redTeam[2].split('c')[1];
+
+    timerView.bumperColor = 'blue';
+  }
+
+  var allyString = ally1 + ' ' + ally2 + ' ' + ally3;
+  var oppoString = oppo1 + ' ' + oppo2 + ' ' + oppo3;
+
+  timerView.allies = allyString;
+  timerView.oppo = oppoString;
+
+  var schedule = data.container.schedule;
+
+  schedule.forEach(function (e) {
+    var tmpDate = new Date(e.time * 1000);
+    var matchString = tmpDate.toLocaleTimeString([], {
       hour: '2-digit',
       minute: '2-digit'
     });
-    //document.querySelector('timer-view').predictedTime = predictedDate.toLocaleTimeString();
+    e.timeString = matchString;
+  });
 
-    var redTeam = data.currentMatch.redTeams;
-    var blueTeam = data.currentMatch.blueTeams;
+  scheduleView.data = schedule;
+}
 
-    var redPos = redTeam.indexOf('frc401');
-    var bluePos = blueTeam.indexOf('frc401');
+function generalContainerHandler(data, streamView) {
+  streamView.streamType = data.streamType;
+  streamView.video = data.video; //<------------THIS WILL PROBABLY NEED A HELPER....
+}
 
-    var ally1, ally2, ally3, oppo1, oppo2, oppo3 = '';
-
-    if (redPos != -1) {
-      ally1 = redTeam[0].split('c')[1];
-      ally2 = redTeam[1].split('c')[1];
-      ally3 = redTeam[2].split('c')[1];
-
-      oppo1 = blueTeam[0].split('c')[1];
-      oppo2 = blueTeam[1].split('c')[1];
-      oppo3 = blueTeam[2].split('c')[1];
-
-      document.querySelector('timer-view').bumperColor = 'red';
-    } else if (bluePos != -1) {
-      ally1 = blueTeam[0].split('c')[1];
-      ally2 = blueTeam[1].split('c')[1];
-      ally3 = blueTeam[2].split('c')[1];
-
-      oppo1 = redTeam[0].split('c')[1];
-      oppo2 = redTeam[1].split('c')[1];
-      oppo3 = redTeam[2].split('c')[1];
-
-      document.querySelector('timer-view').bumperColor = 'blue';
-    }
-
-    var allyString = ally1 + ' ' + ally2 + ' ' + ally3;
-    var oppoString = oppo1 + ' ' + oppo2 + ' ' + oppo3;
-
-    document.querySelector('timer-view').allies = allyString;
-    document.querySelector('timer-view').oppo = oppoString;
-
-    var schedule = messageObj.payload.container.schedule;
-
-    schedule.forEach(function (e) {
-      var tmpDate = new Date(e.time * 1000);
-      var matchString = tmpDate.toLocaleTimeString([], {
-        hour: '2-digit',
-        minute: '2-digit'
-      });
-      e.timeString = matchString;
-    });
-
-    document.querySelector('schedule-view').data = schedule;
-    break;
-  case 'TV_SET':
-    var data = messageObj.payload;
-    console.log(data);
-    var tv = document.querySelector('tv-app');
-    console.log(tv.screen);
-    if (tv.screen == data.name) {
-      console.log('name matched!')
-      tv.set('page', data.selected);
-      //powerHandlder(data.power);
-      //muteHandler(data.mute);
-      //volumeHandler(data.volume)
-    }
-
-    break;
+function tvHandler(data, tv) {
+  if (tv.screen == data.name) {
+    tv.set('page', data.selected);
+    setVolume(data.volume);
   }
-});
+}
 
 function powerHandler() {
   //memes
+}
+
+function setVolume(newVol) {
+  vol.set(newVol, function (err) {});
 }
